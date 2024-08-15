f9cefa9ca75edafab341bb33f559d99f61954aae
gustav0tx <gustavo.goes1@aluno.ce.gov.br>
2024-08-14 22:04:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// nome completo
+let nome = prompt('Qual é o seu nome?');
+let sobrenome = prompt ('E seu sobrenome?');
+// idade 
+let idade = prompt('Qual é a seu ano de nascimento?');
+
+//Curso 
+let curso = prompt('Qual é o seu curso?');
+alert ('Olá' + ' ' + nome + ' ' + sobrenome + '\n' + 'Então você estuda' + ' ' + curso + '\n' + 'e tem' + ' ' + (2024 - idade)  + ' ' + 'anos de idade.' + ' ' + 'legal')
